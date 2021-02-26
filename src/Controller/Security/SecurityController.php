dc6026fd38d3e34181dea89d618ceffede8d426d
Max <maximsa@inbox.lv>
2021-02-26 17:00:19 +0200

    Design update

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -14,16 +14,16 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $title = 'Login page';
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'title' => $title]);
     }
 
     /**
