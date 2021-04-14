a5b964928b651977987bd8c3a1f54342187b6f70
Max <maximsa@inbox.lv>
2021-04-14 21:56:40 +0300

    Forms were made

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Category name',
+                'attr' => [
+                    'placeholder' => 'Enter category name'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Category description',
+                'attr' => [
+                    'placeholder' => 'Enter description'
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Category image',
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save category',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+            ->add('delete', SubmitType::class, [
+                'label' => 'Delete category',
+                'attr' => [
+                    'class' => 'btn btn-danger'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Post title',
+                'attr' => [
+                    'placeholder' => 'Enter post title'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Post content',
+                'attr' => [
+                    'placeholder' => 'Enter text'
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Post image',
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Select category',
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Add post',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+            ->add('delete', SubmitType::class, [
+                'label' => 'Delete post',
+                'attr' => [
+                    'class' => 'btn btn-danger'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
